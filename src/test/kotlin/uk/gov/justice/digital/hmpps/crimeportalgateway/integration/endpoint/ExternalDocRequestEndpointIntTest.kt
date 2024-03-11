a6b460d3bad1ce16d2812488b2fe27c75477e348
Mark Rees <mark.rees@digital.justice.gov.uk>
2024-03-11 09:28:09 +0000

    PIC-2929 ktlint fix

--- src/test/kotlin/uk/gov/justice/digital/hmpps/crimeportalgateway/integration/endpoint/ExternalDocRequestEndpointIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/crimeportalgateway/integration/endpoint/ExternalDocRequestEndpointIntTest.kt
@@ -67,7 +67,6 @@ class ExternalDocRequestEndpointIntTest : IntegrationTestBase() {
 
         val expectedMessageDetail = MessageDetail(courtCode = "B10JQ", courtRoom = 0, hearingDate = "2020-10-26")
 
-
         // possibly check S3 upload
         // definitely get sqs message
     }
