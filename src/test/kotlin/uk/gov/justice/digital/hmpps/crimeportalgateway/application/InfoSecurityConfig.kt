ecd2496374ad1492111fe87e0b86cf167400cfe2
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2024-09-16 10:58:03 +0100

    Disable CSRF on info endpoint for testing
    The info endpoint does not work in prod/pre-prod/dev via the Ingress,
    which adds a prefix to all endpoints.
    
    It also fails to work due to csrf protection, which causes our tests
    to fail.
    
    Disable CSRF on the info endpoint for tests only.
    
    We have a ticket to fix the /info and /health endpoints in other
    environments.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/crimeportalgateway/application/InfoSecurityConfig.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/crimeportalgateway/application/InfoSecurityConfig.kt
@@ -5,9 +5,11 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.test.context.ActiveProfiles
 
 @Configuration
 @EnableWebSecurity
+@ActiveProfiles("test")
 class InfoSecurityConfig {
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
