1e6d486f9a262bd0462dc43178740630be06b9d9
Marcus Aspin <maspin@unilink.com>
2026-05-12 16:46:37 +0100

    Upgrade HMPPS Gradle plugin to 10.2.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.2.1"
+    id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.2.5"
     kotlin("plugin.spring") version "2.3.20"
     kotlin("jvm") version "2.3.20"
 }
